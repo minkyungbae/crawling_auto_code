577c93727f03c55beadb76762a9a5542e98e44a2
민경쓰 <mac@macui-MacBookPro.local>
2025-05-16 16:18:44 +0900

    Add: Swagger code

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Youtube Crawling API",
+        default_version='v1',
+        description="유튜브 크롤링 API 문서",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/crawling/', include('youtube_crawling.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI (선택)
 ]
