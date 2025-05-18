8ebc043edfeb72f48ee477969b9f090994d0b628
minkyungbae <bmk0703@naver.com>
2025-05-19 00:54:53 +0900

    Fix: Video Model(almost Fields change CharField)

--- youtube_crawling/models.py
+++ youtube_crawling/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class YouTubeVideo(models.Model):
-    video_id = models.CharField(max_length=20, unique=True)
+    video_id = models.CharField(max_length=255, unique=True)
     extracted_date = models.DateField()
-    upload_date = models.DateField()
+    upload_date = models.CharField(max_length=255)
     channel_name = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     video_url = models.URLField(unique=True)  # 중복 방지
-    subscriber_count = models.CharField(max_length=50)
-    view_count = models.CharField(max_length=50)
-    product_count = models.PositiveIntegerField(null=True, blank=True)
+    subscriber_count = models.CharField(max_length=255)
+    view_count = models.CharField(max_length=255)
+    product_count = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
@@ -18,10 +18,10 @@ class YouTubeVideo(models.Model):
 
 class YouTubeProduct(models.Model):
     video = models.ForeignKey(YouTubeVideo, related_name='products', on_delete=models.CASCADE)
-    product_image_link = models.URLField(null=True, blank=True)
+    product_image_link = models.CharField(max_length=255)
     product_name = models.CharField(max_length=255)
     product_price = models.CharField(max_length=100)
-    product_link = models.URLField(null=True, blank=True)
+    product_link = models.CharField(max_length=255)
 
     def __str__(self):
         return self.product_name
