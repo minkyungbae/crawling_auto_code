2b7302826d849569a55f5f127def7a41e82239b5
민경쓰 <bmk0703@naver.com>
2025-05-22 00:31:54 +0900

    Add: def ready(celery beat schedule code)

--- youtube_crawling/apps.py
+++ youtube_crawling/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class YoutubeCrawlingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'youtube_crawling'
+
+    def ready(self):
+        from . import schedule_code
+        schedule_code.setup_periodic_tasks()
