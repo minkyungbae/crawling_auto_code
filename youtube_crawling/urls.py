83c7c27d41c3c75b12b2ee60e08888df04b8e955
민경쓰 <bmk0703@naver.com>
2025-05-21 23:42:24 +0900

    Add: Channel Trigger url

--- youtube_crawling/urls.py
+++ youtube_crawling/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from youtube_crawling.views.longform_views import (
     YoutubeLongFormCrawlAPIView,
     YouTubeVideoOneAPIView,
+    ChannelCrawlTriggerView
     )
 
 urlpatterns = [
     path('longform/', YoutubeLongFormCrawlAPIView.as_view()), # 여러 개 영상 크롤링 (C,R,PUT,D)
     path('longform/<str:video_id>/', YouTubeVideoOneAPIView.as_view()), # 특정 영상 한 개 (R,D,PATCH)
+    path('channel/', ChannelCrawlTriggerView.as_view()), # 유튜브 채널에 있는 영상 크롤링 (POST)
 ]
