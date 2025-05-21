0cae1beef9df0eca70a0c65af83df4f9daabb3cd
민경쓰 <bmk0703@naver.com>
2025-05-21 10:59:28 +0900

    Add: API url

--- youtube_crawling/urls.py
+++ youtube_crawling/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
-from youtube_crawling.views.longform_views import YoutubeLongFormCrawlAPIView
+from youtube_crawling.views.longform_views import (
+    YoutubeLongFormCrawlAPIView,
+    YouTubeVideoViewSet,
+    YouTubeProductViewSet
+    )
+
 from rest_framework.routers import DefaultRouter
-from .views.longform_views import YouTubeVideoViewSet
 
 router = DefaultRouter()
 router.register(r'youtube-videos', YouTubeVideoViewSet)
+router.register(r'youtube-products', YouTubeProductViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-]
-
-urlpatterns = [
-    path('longform/', YoutubeLongFormCrawlAPIView.as_view()),
+    path('', include(router.urls)), # ViewSet API (GET, POST, PUT, DELETE 등 자동 라우팅)
+    path('longform/', YoutubeLongFormCrawlAPIView.as_view()), # POST /longform/ => 크롤링
 ]
