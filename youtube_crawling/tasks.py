590fb471d8ed005839727d15e170694db087fb12
민경쓰 <bmk0703@naver.com>
2025-05-26 21:52:20 +0900

    Fix: crawl_chnannel_videos

--- youtube_crawling/tasks.py
+++ youtube_crawling/tasks.py
@@ -9,7 +9,7 @@ def crawl_channels_task():
     channel_urls = [
         "https://www.youtube.com/@%EC%B9%A1%EC%B4%89",
     ]
-    export_dir = "exports"
+    export_dir = "./crawling_result_csv/"
     os.makedirs(export_dir, exist_ok=True)
 
     today_str = datetime.datetime.now().strftime("%Y%m%d")
@@ -19,9 +19,11 @@ def crawl_channels_task():
             logger.info(f"🚀 채널 시작: {url}")
 
             # 채널 이름 추출 및 디코딩
-            channel_name = urllib.parse.unquote(url.split("/")[-1])
-            save_path = os.path.join(export_dir, f"{channel_name}_{today_str}.xlsx")
-            crawl_channel_videos(url, save_path)
+            raw_channel_name = urllib.parse.unquote(url.split("/")[-1])
+            format_channel_name = "".join(
+                c for c in raw_channel_name.replace(" ", "_") if c.isalnum() or c in ('_',)
+            ).rstrip()
+            crawl_channel_videos(url, export_dir, format_channel_name)
             
             logger.info(f"✅ 채널 완료: {url}")
         except Exception as e:
