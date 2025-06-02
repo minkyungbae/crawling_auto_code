70eee4be2c7c93d38a038de29acaab333968bc0e
minkyungbae <bmk0703@naver.com>
2025-06-02 11:21:09 +0900

    Fix: url. 'api/v1/crawling/' -> 'api/v1/crawl/longform'

--- config/urls.py
+++ config/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/crawling/', include('youtube_crawling.urls')),
+    path('api/v1/crawl/longform/', include('youtube_crawling.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
     # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI (선택)
 ]
